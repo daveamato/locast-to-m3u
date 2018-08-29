86f72b688d76001c0ee8b7022440c21089abd676
Mike DiGioia <mdigioia@aps.org>
2018-08-29 10:34:01 -0400

    Updated

--- ripper.js
+++ ripper.js
@@ -1,7 +1,11 @@
 const fs = require('fs')
+const fetch = require('node-fetch')
+const FormData = require('form-data')
+const { URLSearchParams } = require('url')
 
-const lat = process.env.LAT || 40.768437
-const lon = process.env.LON || -73.0107316
+
+const lat = process.env.LAT || "40.768437"
+const lon = process.env.LON || "-73.0107316"
 const username = process.env.USERNAME
 const password = process.env.PASSWORD
 const targetPath = process.env.TARGET_PATH ||"./tv-source.m3u"
@@ -20,76 +24,105 @@ const channelConfig = {
   WNJU: { zapId: "I16.11726.zap2it.com", channelNumber: 31 },
   WLNY: { zapId: "I710.31225.zap2it.com", channelNumber: 55 },
   WFUT: { zapId: "I1005.35364.zap2it.com", channelNumber: 68 },
-  NYLIFE: { zapId: "I22.11744.zap2it.com", channelNumber: 25 }
+  NYLIFE: { zapId: "I22.11744.zap2it.com", channelNumber: 27 }
 }
 
-
-function request(result) {
-  let headers = ['user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36']
+function mapObj(obj, func){
+  return Object.keys(obj).map((key) => func([key, obj[key]]))
+}
+function getHeaders(result) {
+  let headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'}
   if (result && result.cookie) {
-    headers = [...headers, `cookie: ${result.cookie}; _member_location=${lat}%2C${lon}`]
+    headers = {...headers, cookie: `${result.cookie}; _member_location=${lat}%2C${lon}`}
   }
-  return new (require('curl-request'))().setHeaders(headers)
+  return headers
 }
+
 async function fetchLocastAuthCookie (result) {
-  try {
-    let { statusCode, body, headers } = await request(result).setBody(
-      {
-        username,
-        password,
-        action: "member_login"
-      }
-    ).post(adminUrl)
-    return { cookie: `_member_token=${body.token}; _member_role=1; _member_username=${username}`}
-  } catch (e) {
-    console.log("Couldn't fetch token");  
-  }
+  const form = new FormData();
+  form.append('username', username);
+  form.append('password', password);
+  form.append('action', "member_login");
+
+  const res = await fetch(adminUrl, {
+    method: "POST", 
+    headers: {... getHeaders(result), ...form.getHeaders()},
+    body: form
+  })
+  const body = await res.json()
+  return { cookie: `_member_token=${body.token}; _member_role=1; _member_username=${username}`}
 }
+
 function fetchChannelData(result) {
   return async function (targetChannel){
+    const params = new URLSearchParams({
+      action: "get_station",
+      station_id: targetChannel.id,
+      lon,
+      lat
+    });
+    const res = await fetch(`${adminUrl}?${params}`, {headers: getHeaders(result)})
+    const body = await res.json()
+    const streamReq = await fetch(body.streamUrl, {headers: getHeaders(result)})
+    const targetSourcesText = await streamReq.text()
+    const targetSources = targetSourcesText.split("\n")
     
-    const channelInfoUrl = `${adminUrl}?action=get_station&station_id=${targetChannel.id}&lat=${lat}&lon=${lon}`
-    let { statusCode, body, headers } = await request(result).get(channelInfoUrl)
-    let m3uSrc = await request(result).get(body.streamUrl)
-    m3uSrc = m3uSrc.body.split("\n")
-    hdSrc = m3uSrc[m3uSrc.length - 2]
-    return { ...targetChannel, streamUrl: body.streamUrl, m3uSrc: hdSrc}
+    const targetSource = targetSources[targetSources.length - 2]
+    return { ...targetChannel, streamUrl: body.streamUrl, targetSource }
   }
 }
+
+function m3u_channelLine (chan) {
+  console.log(chan)
+  const props = mapObj({
+    "channel-id": channelConfig[chan.callSign].channelNumber,
+    "channel-name": chan.callSign,
+    "tvg-name": chan.callSign,
+    "tvg-id": channelConfig[chan.callSign].zapId,
+    "tvg-logo": chan.logoUrl
+  }, ([prop, value])=> `${prop}="${value}"`).join(' ')
+  return [`#EXTINF:-1 ${props}`, chan.targetSource].join('\n')
+}
 function buildM3U(channelList) {
-  return `#EXTM3U
-   ${channelList.filter((chan)=>channelConfig[chan.callSign]).map((chan)=>
-      `
-#EXTINF:-1 channel-id="${channelConfig[chan.callSign].channelNumber}" channel-name="${chan.callSign}" tvg-name="${chan.callSign}" tvg-id="${channelConfig[chan.callSign].id}" tvg-logo="${chan.logoUrl}"
-${chan.m3uSrc}`).join('\n')}
-   `
+  const channelLineup = channelList
+                        .filter((chan) => channelConfig[chan.callSign])
+                        .map(m3u_channelLine)
+  return ['#EXTM3U',...channelLineup].join('\n')
 }
 async function getDMA(result) {
-  let { statusCode, body, headers } = await request(result).get(`${adminUrl}?action=get_dma&lat=${lat}&lon=${lon}`)
+  const params = new URLSearchParams({
+    action: "get_dma",
+    lon,
+    lat
+  });
+  const res = await fetch(`${adminUrl}?${params}`, {
+    headers: getHeaders(result)})
+  const body = await res.json()
   return {...result, dma: body.DMA}
 }
 async function getChannelList(result) {
-  try {
-    const channelListUrl = `${adminUrl}?action=get_epgs&dma=${result.dma}&start_time=2018-08-24T00%3A00%3A00.562-07%3A00`
-    let { statusCode, body, headers } = await request(result).get(channelListUrl)
-    const channelList = await Promise.all(body.map(fetchChannelData(result)))
-    return { ...result, channelList, m3u: buildM3U(channelList) }
-  } catch (e) {
-    console.log(e, "Couldn't fetch channelList");
-  }
+  const params = new URLSearchParams({
+    action: "get_epgs",
+    dma: result.dma,
+    start: Date(),
+    lon,
+    lat
+  });
+  const res = await fetch(`${adminUrl}?${params}`, {headers: getHeaders(result)})
+  const body = await res.json()
+  const channelList = await Promise.all(body.map(fetchChannelData(result)))
+  return { ...result, channelList, m3u: buildM3U(channelList) }
 }
 async function updateOTAFile () {
-  let result = await fetchLocastAuthCookie()
-  result = await getDMA(result)
-  result = await getChannelList(result)
-  return result
-}
-async function updateM3UFile () {
-  console.log("Fetching M3U From source")
-  const { m3u } = await updateOTAFile()
-  console.log("Building M3U")
-  fs.writeFileSync(targetPath, m3u)
-  console.log("SYNC COMPLETE")
+  try {
+    let result = await fetchLocastAuthCookie()
+    result = await getDMA(result)
+    result = await getChannelList(result)
+    fs.writeFileSync(targetPath, result.m3u)
+    return result
+  } catch (e) {
+    console.error(e, "Failure to update OTA File")
+  }
 }
 
-module.exports.init = () => { updateM3UFile(); setInterval(updateM3UFile, updateFreq) }
+module.exports.init = () => { updateOTAFile(); setInterval(updateOTAFile, updateFreq) }
